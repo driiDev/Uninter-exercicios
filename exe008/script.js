f196eed1dfd5cafab04f255aa5f029639c3b897d
Andrielle Soares <andriellesoares76@gmail.com>
2024-05-28 20:29:43 -0300

    add: exe008

---
+++ exe008/script.js
@@ -0,0 +1,12 @@
+let listaDesejos = ["Carro", "Apartamento", "Videogame", "Celular"];
+
+let pessoa = {
+    nome: "Andrielle",
+    idade: 19,
+    lista: listaDesejos
+};
+
+console.log(listaDesejos);
+
+console.log(pessoa);
+
