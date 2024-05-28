d1621c7fcf679ae32058ad084f616881b82dbf83
Andrielle Soares <andriellesoares76@gmail.com>
2024-05-28 20:16:22 -0300

    add: exe007

---
+++ exe007/script.js
@@ -0,0 +1,12 @@
+console.log("Olá, mundo!");
+console.log("4 + 4 é " + (4 + 4));
+
+const nome = "Andrielle";
+let idade = 19;
+
+if(idade >= 18){
+    console.log(`Olá ${nome}. Sua idade é ${idade}.`);
+} else{
+    console.log("Acesso bloqueado.");
+}
+
