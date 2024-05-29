bbaec0d7156d143a349aba94e2b07d8d6bf31880
Andrielle Soares <andriellesoares76@gmail.com>
2024-05-29 15:42:10 -0300

    feat: botão removido e resposta automaticamente calculada

--- exe009/002/script.js
+++ exe009/002/script.js
@@ -1,20 +1,16 @@
 const campo1 = document.querySelector("#campo1");
 const campo2 = document.querySelector("#campo2");
 const seletor = document.querySelector("#operacao");
-const botao = document.querySelector("#igual");
 let resposta = document.querySelector("#resposta");
 
-botao.addEventListener("click", calcular);
+seletor.addEventListener("change", calcular);
+campo1.addEventListener("keyup", calcular);
+campo2.addEventListener("keyup", calcular);
+
 
 function calcular(){
-    if(campo1.value === '' || campo2.value === ''){
-        resposta.classList.add("problema");
-        resposta.innerHTML = "Campo vazio!!";
-        setTimeout(()=>{
-            resposta.classList.remove("problema");
-            resposta.innerHTML = '';
-        },3000);
-    }else{
+    if(campo1.value!= '' && campo2.value!= ''){
+        
     const valor1 = parseInt(campo1.value);
     const valor2 = parseInt(campo2.value);
     const operacao = seletor.value;
